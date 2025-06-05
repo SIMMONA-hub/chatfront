f4ea0a8a550b9dadae2a221a3a1ef5b11b698a1e
SIMMONA <splantflower@gmail.com>
2025-06-05 23:34:02 +0500

    Настройка для GitHub Pages деплоя

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
- import { defineConfig } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-})
+  base: '/chatfrontend/', // замените на название вашего репозитория
+  build: {
+    outDir: 'dist',
+  }
+})      
